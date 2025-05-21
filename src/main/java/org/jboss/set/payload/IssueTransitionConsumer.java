229b23049b6b5b7fc893cf1198d9d8b93c2f568e
Tomas Hofman <thofman@redhat.com>
2025-05-21 17:16:34 +0200

    Modify the Jira messages to also include fix version

--- src/main/java/org/jboss/set/payload/IssueTransitionConsumer.java
+++ src/main/java/org/jboss/set/payload/IssueTransitionConsumer.java
@@ -10,8 +10,8 @@ public class IssueTransitionConsumer extends AbstractIssueConsumer {
 
     private static final Logger logger = Logger.getLogger(IssueTransitionConsumer.class);
 
-    private static final String COMPONENT_UPGRADE_MESSAGE = "This component upgrade has been incorporated in manifest[*].\n\n[*] %s";
-    private static final String INCORPORATED_ISSUE_MESSAGE = "This issue has been incorporated in manifest[*] via component upgrade %s.\n\n[*] %s";
+    private static final String COMPONENT_UPGRADE_MESSAGE = "This component upgrade has been incorporated in manifest marked as %s [*\\].\n\n[*\\] %s";
+    private static final String INCORPORATED_ISSUE_MESSAGE = "This issue has been incorporated in manifest [*\\] marked as %s via component upgrade %s.\n\n[*\\] %s";
     private static final String LABEL = "on-payload";
 
     private final Collection<String> fixVersions;
@@ -24,7 +24,7 @@ public class IssueTransitionConsumer extends AbstractIssueConsumer {
 
     @Override
     public void componentUpgradeIssue(Issue issue, String manifestReference) {
-        String comment = String.format(COMPONENT_UPGRADE_MESSAGE, manifestReference);
+        String comment = String.format(COMPONENT_UPGRADE_MESSAGE, fixVersions, manifestReference);
         if (!hasLabel(issue, LABEL)) {
             issueClient.addComment(issue, comment);
             issueClient.updateIssue(issue, fixVersions, LABEL);
@@ -36,7 +36,7 @@ public class IssueTransitionConsumer extends AbstractIssueConsumer {
 
     @Override
     public void incorporatedIssue(Issue issue, Issue componentUpgrade, String manifestReference) {
-        String comment = String.format(INCORPORATED_ISSUE_MESSAGE, componentUpgrade.getKey(), manifestReference);
+        String comment = String.format(INCORPORATED_ISSUE_MESSAGE, fixVersions, componentUpgrade.getKey(), manifestReference);
         if (!hasLabel(issue, LABEL)) {
             issueClient.addComment(issue, comment);
             issueClient.updateIssue(issue, fixVersions, LABEL);
